70e3cc7b1a70dbc757de2f448d6b14be2ddf9d43
Katalinn <ka.petraskova@gmail.com>
2018-10-08 23:40:13 +0200

    * ukol - create a simple program

---
+++ 002_MidnightSnack/src/MidnightSnack.java
@@ -0,0 +1,8 @@
+public class MidnightSnack {
+    public static void main (String [] args){
+
+        /* My second program made around midnight */
+        System.out.println("I wanna Midnight Snack!");
+        System.out.println("Do you wanna some too?");
+    }
+}

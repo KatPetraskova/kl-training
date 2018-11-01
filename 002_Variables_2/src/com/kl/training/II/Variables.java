63e02a3cf868bdcc5a9ba38c0224ac79cc9c47fe
Katalinn <ka.petraskova@gmail.com>
2018-11-01 11:30:27 +0100

    * ukoly z dokončení 2. lekce - Vylet materske skolky do ZOO

---
+++ 002_Variables_2/src/com/kl/training/II/Variables.java
@@ -0,0 +1,31 @@
+package com.kl.training.II;
+
+/**
+ * @author Katka
+ * This is basic documentation for Variables class
+ * 01. 11. 2018
+ */
+
+public class Variables {
+
+    /**
+     * This is main method which serves as start of program "Vylet materske skolky do ZOO"
+     * @param args - input arguments
+     */
+
+    public static void main (String [] args) {
+
+        /* Vylet materske skolky do ZOO */
+
+    String[] mhd = new String[5];
+
+    mhd[0] = "paní učitelka";
+    mhd[1] = "Honzík";
+    mhd[2] = "Vašík";
+    mhd[3] = "Lenka";
+    mhd[4] = "Mája";
+
+    System.out.println ("Jméno dítěte na posledním místě: " + mhd[4]);
+
+}
+}
